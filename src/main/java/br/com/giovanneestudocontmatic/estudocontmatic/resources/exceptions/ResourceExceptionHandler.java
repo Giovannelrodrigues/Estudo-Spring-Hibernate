41345f7a6adb13384395425efc7886f9fe54f375
giovanne lisboa <giovannelrodrigues@gmail.com>
2022-01-19 13:36:16 -0300

    Excepition personalizada da validation bean

--- src/main/java/br/com/giovanneestudocontmatic/estudocontmatic/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/giovanneestudocontmatic/estudocontmatic/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import br.com.giovanneestudocontmatic.estudocontmatic.services.exceptions.DataIn
 import br.com.giovanneestudocontmatic.estudocontmatic.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,4 +25,15 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Error de Validação", System.currentTimeMillis());
+        for(FieldError fieldError: e.getBindingResult().getFieldErrors()){
+            err.add(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+
 }
